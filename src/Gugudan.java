a9d783bb6964c1d70ffa618024dae8ed9da5506e
82104 <82104@192.168.1.104>
2022-01-19 19:43:10 +0900

    구구단 1

---
+++ src/Gugudan.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Gugudan {
+
+	public static void main(String [] args) {
+	System.out.print("구구단 총 출력할 단은?");
+	Scanner scanner = new Scanner(System.in);
+	int number = scanner.nextInt();
+	System.out.println("number: "+number);
+	
+	number = 4;
+	System.out.println("4단");
+	System.out.println(number*1);
+	System.out.println(number*2);
+	System.out.println(number*3);
+	System.out.println(number*4);
+	System.out.println(number*5);
+	System.out.println(number*6);
+	System.out.println(number*7);
+	System.out.println(number*8);
+	System.out.println(number*9);
+	
+	number = 5;
+	System.out.println("5단");
+	System.out.println(number*1);
+	System.out.println(number*2);
+	System.out.println(number*3);
+	System.out.println(number*4);
+	System.out.println(number*5);
+	System.out.println(number*6);
+	System.out.println(number*7);
+	System.out.println(number*8);
+	System.out.println(number*9);
+	}
+}
